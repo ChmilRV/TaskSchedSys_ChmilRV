d48049cf5c0ea79d4c2dbd5a59b3a0de01e8033e
Chmil Ruslan <chmilrv@users.noreply.github.com>
2020-09-26 15:52:43 +0300

    1552

--- TaskSchedulingSystem_ChmilRV/Program.cs
+++ TaskSchedulingSystem_ChmilRV/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
+using static System.Console;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 /*Реализуйте с использованием паттернов проектирования простейшую систему планирования задач.
 Должна быть возможность создания списка дел, установки приоритетов,
 установки дат выполнения, удаление и изменения дел. Каждому делу можно установить тег.
@@ -11,17 +9,76 @@ using System.Threading.Tasks;
 Критерии поиска: по датам, по тегам, по приоритету и так далее.*/
 namespace TaskSchedulingSystem_ChmilRV
 {
-
+    public interface IBuilder
+    {
+        void BuildPartA();
+        void BuildPartB();
+        void BuildPartC();
+    }
+    public class ConcreteBuilder : IBuilder
+    {
+        private Product product = new Product();
+        public ConcreteBuilder() { Reset(); }
+        public void Reset() { product = new Product(); }
+        public void BuildPartA() { product.Add("PartA1"); }
+        public void BuildPartB() { product.Add("PartB1"); }
+        public void BuildPartC() { product.Add("PartC1"); }
+        public Product GetProduct()
+        {
+            Product result = product;
+            Reset();
+            return result;
+        }
+    }
+    public class Product
+    {
+        private List<object> parts = new List<object>();
+        public void Add(string part) { parts.Add(part); }
+        public string ListParts()
+        {
+            string str = string.Empty;
+            for (int i = 0; i < parts.Count; i++) { str += parts[i] + ", "; }
+            str = str.Remove(str.Length - 2);
+            return "Product parts: " + str + "\n";
+        }
+    }
+    public class Director
+    {
+        private IBuilder builder;
+        public IBuilder Builder { set { builder = value; } }
+        public void buildMinimalViableProduct() { builder.BuildPartC(); }
+        public void buildFullFeaturedProduct()
+        {
+            builder.BuildPartA();
+            builder.BuildPartB();
+            builder.BuildPartC();
+        }
+    }
 
     class Program
     {
-
-
         static void Main(string[] args)
         {
-
-
-
+            // порождающий - строитель
+            // структурный - компоновщик
+            Director director = new Director();
+            ConcreteBuilder builder = new ConcreteBuilder();
+            director.Builder = builder;
+            WriteLine("Standard basic product:");
+            director.buildMinimalViableProduct();
+            WriteLine(builder.GetProduct().ListParts());
+            WriteLine("Standard full featured product:");
+            director.buildFullFeaturedProduct();
+            WriteLine(builder.GetProduct().ListParts());
+            WriteLine("Custom product:");
+            builder.BuildPartA();
+            builder.BuildPartC();
+            builder.BuildPartB();
+            builder.BuildPartA();
+            builder.BuildPartC();
+            builder.BuildPartA();
+            Write(builder.GetProduct().ListParts());
+            ReadKey();
         }
     }
 }
